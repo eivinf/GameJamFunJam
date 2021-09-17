a7fd13b5d4f7fe32bc82fdfff7f8aed341aac9c4
Tobias Ørstad <tobiaso56@gmail.com>
2021-09-17 20:41:21 +0200

    add shit

--- Assets/CopyPosition.cs
+++ Assets/CopyPosition.cs
@@ -6,11 +6,25 @@ public class CopyPosition : MonoBehaviour
 {
     public Transform target;
     public float force;
+    public bool isSwinging;
 
-    void FixedUpdate()
+    void Update()
     {
-        //GetComponent<Rigidbody>().AddForce((target.position - transform.position) * force);
-        transform.position = target.position;
-        GetComponent<Rigidbody>().velocity = target.GetComponent<Rigidbody>().velocity;
+        if (Input.GetMouseButtonDown(0) && !isSwinging) {
+            target.position = transform.position;
+            isSwinging = true;
+            target.GetComponent<Rigidbody>().velocity = Vector3.zero;
+         }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isSwinging = false;
+        }
+        if (isSwinging)
+        {
+            //GetComponent<Rigidbody>().AddForce((target.position - transform.position) * force);
+            transform.position = target.position;
+            GetComponent<Rigidbody>().velocity = target.GetComponent<Rigidbody>().velocity;
+        }
+        
     }
 }
