49615577fec20b99a152c6eb6ac5c2d44dea4b23
eivinef <eivinef@stud.ntnu.no>
2021-09-17 21:21:17 +0200

    Add placement of anchor

--- Assets/Scripts/CopyPosition.cs
+++ Assets/Scripts/CopyPosition.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class CopyPosition : MonoBehaviour
 {
     public Transform target;
+    public Transform anchor;
     public float force;
     public bool isSwinging;
+    public Transform camera;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && !isSwinging) {
-            target.position = transform.position;
-            isSwinging = true;
-            target.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+            var hit = new RaycastHit();
+            if (Physics.Raycast(camera.position, camera.forward, out hit, 1000))
+            {
+                anchor.position = hit.point;
+                target.position = transform.position;
+                isSwinging = true;
+                target.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
          }
         if (Input.GetMouseButtonUp(0))
         {
