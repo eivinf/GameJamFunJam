fb782dd39a1ea169879e5dee4ffa5a29e3f3c777
magnus <magnurr@stud.ntnu.no>
2021-09-18 14:35:44 +0200

    Update RopeDangling.cs

--- Assets/Scripts/RopeDangling.cs
+++ Assets/Scripts/RopeDangling.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Script based on video by Sebastian Lague
-    public Point[] points;
-    public Stick[] sticks;
-    private float gravity = 9.81f; 
-    private int numIterations = 10;
+    public const float gravity = 9.81f; 
+    public const int numIterations = 10;
+    public const int nodeCount = 16;
+
     public class Point
     {
         public Vector3 position, prevPosition;
@@ -19,10 +19,23 @@ public class NewBehaviourScript : MonoBehaviour
         public Point pointA, pointB;
         public float length;
     }
-
-    void Simulate()
+    public class Rope
     {
-        foreach (Point point in points)
+        public Point anchorPoint;
+        public Point endPoint;
+        public Point[] points;
+        public Stick[] sticks;
+        public float ropeLength;
+    }
+
+    public Rope MakeNewRope(Vector3 anchorPosition, Vector3 endPosition)
+    {
+        return null;
+    }
+
+    public void GetNewNodePositions(Rope rope)
+    {
+        foreach (Point point in rope.points)
         {
             if (!point.locked)
             {
@@ -34,19 +47,20 @@ public class NewBehaviourScript : MonoBehaviour
         }
         for (int i = 0; i < numIterations; i++)
         {
-            foreach (Stick stick in sticks)
+            foreach (Stick stick in rope.sticks)
+            {
+                Vector3 stickCentre = (stick.pointA.position + stick.pointB.position) / 2;
+                Vector3 stickDir = (stick.pointA.position - stick.pointB.position).normalized;
+                if (!stick.pointA.locked)
+                {
+                    stick.pointA.position = stickCentre + stickDir * stick.length / 2;
+                }
+                if (!stick.pointB.locked)
+                {
+                    stick.pointB.position = stickCentre - stickDir * stick.length / 2;
+                }
+            }
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
