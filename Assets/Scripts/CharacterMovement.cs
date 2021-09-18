8c0e70d3cda64ba538efd9d12f2f9801a6d3a5af
eivinef <eivinef@stud.ntnu.no>
2021-09-18 02:38:37 +0200

    initial test

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float maxVerticalMovementSpeed;
     public float verticalMovementSpeed;
     public float horizontalMovementSpeed;
-    public float maxHorizontalMovementSpeed;
-    public Vector3 desireMove;
+
+
+    public float verticalAirMovementSpeed;
+    public float horizontalAirMovementSpeed;
+
+    public Vector3 velocity;
     public Transform anchor;
+    public float minRopeLength;
     public float ropeLength;
-    public float ropeForce;
     public float ropeStretchForce;
     public float gravity;
     public bool isSwinging;
     public Transform camera;
+    public bool grounded;
+    public int CollisionItterations;
+    public float airDrag;
+    public float groundDrag;
+    public float ReelInSpeed;
+    public LayerMask collisionMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,25 +36,59 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>().velocity += Vector3.down * gravity;
 
-        //GetComponent<Rigidbody>().velocity -= desireMove;
-        if (Mathf.Abs(Vector3.Dot(transform.rotation * Vector3.forward, GetComponent<Rigidbody>().velocity)) < maxVerticalMovementSpeed)
+        if (Input.GetMouseButton(1) && ropeLength > minRopeLength)
         {
-            GetComponent<Rigidbody>().velocity += transform.rotation * Vector3.forward * verticalMovementSpeed * Input.GetAxis("Vertical");
+            ropeLength -= ReelInSpeed * Time.deltaTime;
+            ropeLength = Mathf.Max(ropeLength, minRopeLength);
         }
-        if (Mathf.Abs(Vector3.Dot(transform.rotation * Vector3.right, GetComponent<Rigidbody>().velocity)) < maxHorizontalMovementSpeed)
+        else
         {
-            GetComponent<Rigidbody>().velocity += transform.rotation * Vector3.right * horizontalMovementSpeed * Input.GetAxis("Horizontal");
+            velocity += Vector3.down * gravity;
         }
 
-        var moveTowardsAnchor = Mathf.Max(Vector3.Dot(GetComponent<Rigidbody>().velocity, (anchor.position - transform.position)), 0f);
+        if (grounded)
+        {
+            velocity += transform.rotation * Vector3.forward * verticalMovementSpeed * Input.GetAxis("Vertical");
+            velocity += transform.rotation * Vector3.right * horizontalMovementSpeed * Input.GetAxis("Horizontal");
+        }
+        else
+        {
+            velocity += transform.rotation * Vector3.forward * verticalAirMovementSpeed * Input.GetAxis("Vertical");
+            velocity += transform.rotation * Vector3.right * horizontalAirMovementSpeed * Input.GetAxis("Horizontal");
+        }
+
+        var moveTowardsAnchor = Mathf.Min(Vector3.Dot(velocity, (anchor.position - transform.position).normalized), 0f);
         if (Vector3.Distance(anchor.position, transform.position) > ropeLength && isSwinging)
         {
-            GetComponent<Rigidbody>().velocity -= (anchor.position - transform.position).normalized* moveTowardsAnchor * ropeForce;
-            GetComponent<Rigidbody>().velocity += (anchor.position - transform.position).normalized * ((anchor.position - transform.position).magnitude - ropeLength) * ropeStretchForce;
+            velocity -= (anchor.position - transform.position).normalized * moveTowardsAnchor;
+            var ropeStrech = Mathf.Max((anchor.position - transform.position).magnitude - ropeLength, 0f);
+            velocity += (anchor.position - transform.position).normalized * ropeStrech * ropeStretchForce;
 
         }
+       
+        //transform.position += desireMove;
+        //desireMove -= (anchor.position - transform.position).normalized * Vector3.Dot(desireMove, (anchor.position - transform.position));
+
+        grounded = false;
+        for (int i = 0; i < CollisionItterations; i++)
+        {
+            CheckCollision();
+        }
+
+        if (grounded)
+        {
+            velocity *= (1f - groundDrag);
+        }
+        else
+        {
+            velocity *= (1f - airDrag);
+        }
+
+        CheckMovedCollision();
+
+        transform.position += velocity * 50 * Time.deltaTime;
+
         if (Input.GetMouseButtonDown(0) && isSwinging)
         {
             isSwinging = false;
@@ -54,18 +98,133 @@ public class CharacterMovement : MonoBehaviour
             var hit = new RaycastHit();
             if (Physics.Raycast(camera.position, camera.forward, out hit, 1000))
             {
-                ropeLength = Vector3.Distance(transform.position, hit.point);
+                ropeLength = Mathf.Max(Vector3.Distance(transform.position, hit.point), minRopeLength);
                 anchor.position = hit.point;
                 isSwinging = true;
             }
         }
-        
 
-        if (Input.GetMouseButton(1))
+
+    }
+
+    public void CheckCollision()
+    {
+        var radius = 0.5f;
+        var point1 = transform.position + Vector3.up * 0.5f;
+        var point2 = transform.position + Vector3.down * 0.5f;
+        var closestColliderToLine = GetClosestColliderToLine(point1, point2, radius);
+        if (closestColliderToLine.Item2 < radius - 0.05f)
         {
-            ropeLength -= 0.01f;
+            if (transform.position.y - 0.5f > closestColliderToLine.Item1.y)
+            {
+                Debug.Log(Time.time);
+                Debug.DrawLine(transform.position, closestColliderToLine.Item1,Color.red, 10f);
+                grounded = true;
+            }
+            transform.position -= closestColliderToLine.Item3 * (0.5f - closestColliderToLine.Item2) * 1.1f;
         }
-        //transform.position += desireMove;
-        //desireMove -= (anchor.position - transform.position).normalized * Vector3.Dot(desireMove, (anchor.position - transform.position));
+        
+    }
+
+    public void CheckMovedCollision()
+    {
+        var radius = 0.5f;
+        var point1 = transform.position + Vector3.up * 0.5f + velocity;
+        var point2 = transform.position + Vector3.down * 0.5f + velocity;
+        var closestColliderToLine = GetClosestColliderToLine(point1, point2, radius);
+        if (closestColliderToLine.Item2 < radius - 0.05f)
+        {
+            if (transform.position.y - 0.5f > closestColliderToLine.Item1.y)
+            {
+                Debug.Log(Time.time);
+                Debug.DrawLine(transform.position, closestColliderToLine.Item1, Color.red, 10f);
+                grounded = true;
+            }
+            velocity = FlattenDirectionForVector(velocity, closestColliderToLine.Item1 - transform.position);
+        }
+
+    }
+
+    public Vector3 FlattenDirectionForVector(Vector3 vector, Vector3 direction)
+    {
+        var projection = Vector3.Dot(vector, direction.normalized) * direction.normalized;
+        return vector - projection;
+    }
+
+    public (Vector3, float, Vector3) GetClosestColliderToLine(Vector3 point1, Vector3 point2, float radius)
+    {
+        var ClosestPointDistance1 = CastRayCastCylinder(point1, point2, 5, 8, radius);
+        var ClosestPointDistance2 = CastRayHalfShphere(-1f, point1, 5, 8, radius);
+        var ClosestPointDistance3 = CastRayHalfShphere(1f , point2, 5, 8, radius);
+        if (ClosestPointDistance1.Item2 <= ClosestPointDistance2.Item2 && ClosestPointDistance1.Item2 <= ClosestPointDistance3.Item2)
+        {
+            return ClosestPointDistance1;
+        }
+
+        if (ClosestPointDistance2.Item2 <= ClosestPointDistance1.Item2 && ClosestPointDistance2.Item2 <= ClosestPointDistance3.Item2)
+        {
+            return ClosestPointDistance2;
+        }
+        return ClosestPointDistance3;
+
+    }
+
+    public (Vector3, float, Vector3) CastRayCastCylinder(Vector3 point1, Vector3 point2, float layers, float pointsInCircle, float radius)
+    {
+        var ClosestPointDistance = (Vector3.zero, Mathf.Infinity, Vector3.zero);
+
+        for (float i = 0; i < layers; i++)
+        {
+            var origin = point1 + (point2 - point1) * (i / (layers -1));
+            for (float e = 0; e < pointsInCircle; e++)
+            {
+                var direction = Quaternion.Euler(0, 360f * (e / pointsInCircle), 0) * Vector3.forward;
+                var hit = new RaycastHit();
+                //Debug.DrawLine(origin, origin + direction * radius, Color.red, 10f);
+                if (Physics.Raycast(origin, direction, out hit, radius, collisionMask))
+                {
+                    if (hit.distance < ClosestPointDistance.Item2)
+                    {
+                        ClosestPointDistance = (hit.point, hit.distance, direction);
+                    }
+                }
+            }
+        }
+        return ClosestPointDistance;
+    }
+
+    public (Vector3, float, Vector3) CastRayHalfShphere(float flip, Vector3 origin, float layers, float pointsInCircle, float radius)
+    {
+        var ClosestPointDistance = (Vector3.zero, Mathf.Infinity, Vector3.zero);
+
+
+        var hit = new RaycastHit();
+        //Debug.DrawLine(origin, origin + Vector3.up* -flip * radius, Color.red, 10f);
+        if (Physics.Raycast(origin, Vector3.up * -flip, out hit, radius, collisionMask))
+        {
+            if (hit.distance < ClosestPointDistance.Item2)
+            {
+                ClosestPointDistance = (hit.point, hit.distance, Vector3.up * -flip);
+            }
+        }
+
+        for (float i = 1; i < layers; i++)
+        {
+            for (float e = 0; e < pointsInCircle; e++)
+            {
+                var direction = Quaternion.Euler(90f * (i / layers) * flip, 0, 0) * Vector3.forward;
+                direction = Quaternion.Euler(0, 360f * (e / pointsInCircle), 0) * direction;
+                
+                //Debug.DrawLine(origin, origin + direction * radius, Color.red, 10f);
+                if (Physics.Raycast(origin, direction, out hit, radius, collisionMask))
+                {
+                    if (hit.distance < ClosestPointDistance.Item2)
+                    {
+                        ClosestPointDistance = (hit.point, hit.distance, direction);
+                    }
+                }
+            }
+        }
+        return ClosestPointDistance;
     }
 }
